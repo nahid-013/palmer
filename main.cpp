d39a6006d5a7862c6df28e1d912584c3199b0671
Nahid Gabibov <gabibovn13@mail.ru>
2025-10-05 10:57:32 +0300

    clean

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@ struct HttpRequest {
 };
 
 
-// ВСПОМОГАТЕЛЬНЫЕ 
 std::string make_response(int status_code, const std::string& status_text, const std::string& content_type, const std::string& body) {
     std::ostringstream response;
     response << "HTTP/1.1 " << status_code << " " << status_text << "\r\n";
